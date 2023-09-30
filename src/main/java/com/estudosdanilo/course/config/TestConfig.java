d8edf7510cbbde2be62935dee41154f091ffc6a0
Danilo Afonso <danteste0@gmail.com>
2023-09-30 02:46:34 -0300

    Payment, one-to-one association

--- src/main/java/com/estudosdanilo/course/config/TestConfig.java
+++ src/main/java/com/estudosdanilo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudosdanilo.course.entities.Category;
 import com.estudosdanilo.course.entities.Order;
 import com.estudosdanilo.course.entities.OrderItem;
+import com.estudosdanilo.course.entities.Payment;
 import com.estudosdanilo.course.entities.Product;
 import com.estudosdanilo.course.entities.User;
 import com.estudosdanilo.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
